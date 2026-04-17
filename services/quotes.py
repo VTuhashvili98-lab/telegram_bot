a5f97ba0e91266bf88f277c9c38a3d2a894d507d
Valeriy Tuhashvili <vtuhashvili98@mail.ru>
2026-04-17 03:08:58 +0300

    added get_quote by index function

--- services/quotes.py
+++ services/quotes.py
@@ -1,5 +1,5 @@
 import random
-
+import sqlite3
 
 quotes = [
     "Успех — это сумма маленьких усилий, повторяемых изо дня в день.",
@@ -13,3 +13,14 @@ quotes = [
 
 def get_random_quote():
     return random.choice(quotes)
+
+
+def get_quote(user_id: int):
+    with sqlite3.connect("bot.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+        SELECT last_quote_index FROM users WHERE telegram_id = ?
+        """, (user_id,))
+
+        row = cursor.fetchone()
+        return row[0] if row else None
